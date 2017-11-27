32d2accca2462bca498e7dfc873f48c7d53dee5e
jd997 <john.m.durrell@gmail.com>
2017-11-27 08:46:08 -0500

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,52 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] nums = new int[100];
+        setArray1To100(nums);
+        double sum = 0;
+        for (int k=0; k<100; k++) {
+            sum += generateAllNumbers(nums);
+        }
+        System.out.println("When running 100 times, it took an average of " + (sum/100) + " tries to generate every number from 1-100");
+        setArray1To100(nums);
+        for (int k=0; k<1000; k++) {
+            sum += generateAllNumbers(nums);
+        }
+        System.out.println("When running 1,000 times, it took an average of " + (sum/1000) + " tries to generate every number from 1-100");
+        setArray1To100(nums);
+        for (int k=0; k<10000; k++); {
+            sum += generateAllNumbers(nums);
+        }
+        System.out.println("When running 10,000 times, it took an average of " + (sum/10000) + " tries to generate every number from 1-100");
+    }
+
+    public static int generateAllNumbers(int[] numbers) {
+        boolean generateNums = true;
+        int i;
+        for (i=0; generateNums; i++) {
+            generateNums = false;
+            int testNum = (int)(Math.random()*100+1);
+            for (int j=0; j<numbers.length; j++) {
+                if (testNum == numbers[j]) {
+                    numbers[j] = -1;
+                }
+            }
+            if (i>100) {
+                for (int j=0; j<numbers.length; j++) {
+                    if (numbers[j] != -1) {
+                        generateNums = true;
+                    }
+                }
+            } else {
+                generateNums = true;
+            }
+        }
+        return i;
+    }
+
+    public static void setArray1To100(int[] numbers) {
+        for (int i=0; i<100; i++) {
+            numbers[i] = i+1;
+        }
     }
 }
